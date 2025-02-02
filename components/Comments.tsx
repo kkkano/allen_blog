83e53b091dee4f6cb645605fb846604b9ac796a2
wx <657394554@qq.com>
2025-02-03 00:21:03 +0800

    test

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,31 +1,33 @@
 'use client'
 
-import React from 'react'
-import { useTheme } from 'next-themes'
-import Giscus from '@giscus/react'
+import React, { useEffect } from 'react'
 
 const Comments = () => {
-  const { theme, resolvedTheme } = useTheme()
-  const commentsTheme = theme === 'dark' || resolvedTheme === 'dark' ? 'dark' : 'light'
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://giscus.app/client.js'
+    script.setAttribute('data-repo', 'kkkano/allen_blog')
+    script.setAttribute('data-repo-id', 'R_kgDONxOD7A')
+    script.setAttribute('data-category-id', 'DIC_kwDONxOD7M4CmjqM')
+    script.setAttribute('data-mapping', 'pathname')
+    script.setAttribute('data-strict', '1')
+    script.setAttribute('data-reactions-enabled', '1')
+    script.setAttribute('data-emit-metadata', '0')
+    script.setAttribute('data-input-position', 'bottom')
+    script.setAttribute('data-theme', 'light')
+    script.setAttribute('data-lang', 'en')
+    script.setAttribute('crossorigin', 'anonymous')
+    script.async = true
 
-  return (
-    <div className="b-6-pt text-center text-gray-700 dark:text-gray-300">
-      <Giscus
-        id="comments"
-        repo="kkkano/allen_blog"
-        repoId="R_kgDONxOD7A"
-        category="Announcements"
-        categoryId="DIC_kwDONxOD7M4CmjqM"
-        mapping="pathname"
-        strict="1"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="bottom"
-        theme={commentsTheme}
-        lang="en"
-      />
-    </div>
-  )
+    const commentsContainer = document.getElementById('comments-container')
+    if (commentsContainer) commentsContainer.appendChild(script)
+
+    return () => {
+      if (commentsContainer) commentsContainer.innerHTML = ''
+    }
+  }, [])
+
+  return <div id="comments-container" className="b-6-pt text-center text-gray-700 dark:text-gray-300"></div>
 }
 
 export default Comments
