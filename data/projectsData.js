085cf0253e1e00f5e132bf70eeea9756401a2115
wx <657394554@qq.com>
2025-02-02 20:05:02 +0800

    update Index

---
+++ data/projectsData.js
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'A Search Engine',
+    description: `What if you could look up any information in the world? Imagine having access to all human knowledge at your fingertips.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'The Time Machine',
+    description: `A web application that allows users to manage their time more effectively.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData 
