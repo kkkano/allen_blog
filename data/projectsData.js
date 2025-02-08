b2840ee466ffde7bf46875237d189f541a1df87d
wx <657394554@qq.com>
2025-02-09 01:46:01 +0800

    update project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,15 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Imagine having access to all human knowledge at your fingertips.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'AIGC BI',
+    description: `基于 Spring Boot + MQ + AIGC（+ React）的智能数据分析平台。区别于传统 BI，用户只需要导入原始数据集、并输入分析诉求，就能自动生成可视化图表及分析结论，实现数据分析的降本增效。`,
+    imgSrc: 'https://github.com/kkkano/BI/assets/74480939/24038c09-cc84-4ad9-ab3f-1914eed50736',
+    href: 'https://github.com/kkkano/BI',
   },
   {
-    title: 'The Time Machine',
-    description: `A web application that allows users to manage their time more effectively.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'OpenAI-Code-Review',
+    description: `基于 GitHub Actions + OpenAI(ChatGLM) + Git/GitHub + 公众号模板消息实现，然后串联出从代码提交获取通知，Git 检出分支变化，在使用 OpenAI 进行代码和写入日志，再发送消息通知,完成了一个AI代码评审的功能。`,
+    imgSrc: 'https://github.com/user-attachments/assets/8f379984-c035-459e-913b-015f121c6ccd',
+    href: 'https://github.com/kkkano/openai-code-review',
   },
 ]
 
